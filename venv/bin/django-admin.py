eb91750fa7518e3437a87fba6db082c3f51066f7
Murali mahadeva <muralimahadev40@gmail.com>
2020-07-05 19:10:08 +0530

    project complete working fine

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/murali/Documents/Murali/projects/django-react/leads-manager-project/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
